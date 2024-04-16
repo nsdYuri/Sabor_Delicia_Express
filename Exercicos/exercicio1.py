9f7bbe9b367a1e57b9d518ada20e0ba50ac7eb66
Yuri <yurichichedom808@gmail.com>
2024-04-16 13:49:18 -0300

    Realizando primeira lista de exercicios de input, print e formatação de string

---
+++ Exercicos/exercicio1.py
@@ -0,0 +1,22 @@
+# Python na Escola de Programação da Alura
+print("Python na escola de programação Alura.")
+
+#  Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores armazenados em variáveis.
+nome = "Yuri"
+idade = 19
+print(f"Meu nome é {nome} e tenho {idade} anos")
+
+# Imprima a palavra: ‘ALURA’ de modo que cada letra fique em uma linha.
+print("""
+A
+L
+U
+R
+A
+""")
+
+# Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi precisa ser armazenado em uma variável e arredondado para apenas duas casas decimais.
+pi = 3.14159
+print(f"O valod e pi arrendondado é: {pi:.2f}")
+
+
