294730b53c5d6236016540ee4d75a1d8648c0090
Yuri <yurichichedom808@gmail.com>
2024-04-16 13:48:41 -0300

    Criando arquivo app e apresentando opções do menu da aplicação

---
+++ App.py
@@ -0,0 +1,10 @@
+import os
+
+print("𝓢𝓪𝓫𝓸𝓻 𝓓𝓮𝓵𝓲́𝓬𝓲𝓪 𝓔𝔁𝓹𝓻𝓮𝓼𝓼\n")
+
+# Menu de opções 
+print("1. Cadastrar Restaurante")
+print("2. Listar Restaurante")
+print("3. Ativar Restaurante")
+print("4. Sair\n")
+
